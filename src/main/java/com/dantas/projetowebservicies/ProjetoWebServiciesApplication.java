74619805e1146eb14d3781a06bed4ba2b3a73b43
Rafael Dantas <developer.dantas@outlook.com>
2022-01-31 21:56:57 -0300

    Project created

---
+++ src/main/java/com/dantas/projetowebservicies/ProjetoWebServiciesApplication.java
@@ -0,0 +1,13 @@
+package com.dantas.projetowebservicies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServiciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServiciesApplication.class, args);
+	}
+
+}
