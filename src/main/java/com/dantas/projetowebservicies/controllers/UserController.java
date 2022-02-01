52fef40a7155eb4759ad0bae2769fc4f2bd4497d
Rafael Dantas <developer.dantas@outlook.com>
2022-01-31 23:36:16 -0300

    Service layer, component registration

--- src/main/java/com/dantas/projetowebservicies/controllers/UserController.java
+++ src/main/java/com/dantas/projetowebservicies/controllers/UserController.java
@@ -1,18 +1,34 @@
 package com.dantas.projetowebservicies.controllers;
 
 import com.dantas.projetowebservicies.model.user.UserEntity;
+import com.dantas.projetowebservicies.model.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<UserEntity> findAll() {
-        UserEntity user = new UserEntity(1L, "Rafael", "rafael@email.com", "99999999", "12345");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<UserEntity>> findAll() {
+        List<UserEntity> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserEntity> findById(@PathVariable Long id) {
+        UserEntity obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
