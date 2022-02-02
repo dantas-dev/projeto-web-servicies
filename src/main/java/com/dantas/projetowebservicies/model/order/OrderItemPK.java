7d0eb42d55069db2d64536882c68738003458def
Rafael Dantas <developer.dantas@outlook.com>
2022-02-02 18:15:45 -0300

    OrderItem, many-to-many association with extra attributes PART - 1

---
+++ src/main/java/com/dantas/projetowebservicies/model/order/OrderItemPK.java
@@ -0,0 +1,50 @@
+package com.dantas.projetowebservicies.model.order;
+
+import com.dantas.projetowebservicies.model.product.ProductEntity;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private OrderEntity orderEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductEntity productEntity;
+
+    public OrderEntity getOrderEntity() {
+        return orderEntity;
+    }
+
+    public void setOrderEntity(OrderEntity orderEntity) {
+        this.orderEntity = orderEntity;
+    }
+
+    public ProductEntity getProductEntity() {
+        return productEntity;
+    }
+
+    public void setProductEntity(ProductEntity productEntity) {
+        this.productEntity = productEntity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(orderEntity, that.orderEntity) && Objects.equals(productEntity, that.productEntity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderEntity, productEntity);
+    }
+}
